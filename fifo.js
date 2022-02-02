be7cad7adbbd4409a6840de0b825864cb22a0eba
Moina <moina.comoros@yahoo.fr>
2022-02-02 09:16:47 +0400

    Mise à jour avec image et debut prog js

---
+++ fifo.js
@@ -0,0 +1,14 @@
+// Fichier Javascript pour gerer une file FIFO
+
+
+
+//document.getElementById("action1").addEventListener("click",AjoutListe);
+
+function AjoutListe() {
+
+    console.log("je suis dans le javascript");
+    var valeurText = document.getElementById("source").value;
+    console.log("Text Saisie",valeurText)
+    document.getElementById("valeurText").innerHTML = td1 ;
+    //document.getElementById("demo").innerHTML = Date();
+  }

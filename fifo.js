ddd3ce2bc224b208a7822abfc2d023444719bc1e
Moina <moina.comoros@yahoo.fr>
2022-02-03 11:12:27 +0400

    Affiche nom et date sur le HTML

--- fifo.js
+++ fifo.js
@@ -1,14 +1,40 @@
-// Fichier Javascript pour gerer une file FIFO
+// Fichier Javascript pour gerer une file de participant
+
+//console.log("je suis dans le javascript");
 
 
+var participant ={};  // declarer l'Objet qui va contenir le nom, la date, id d'une personne
 
-//document.getElementById("action1").addEventListener("click",AjoutListe);
+//Fonction permettant de saisir le nom et d'ajouter la date 
 
 function AjoutListe() {
-
-    console.log("je suis dans le javascript");
     var valeurText = document.getElementById("source").value;
-    console.log("Text Saisie",valeurText)
-    document.getElementById("valeurText").innerHTML = td1 ;
-    //document.getElementById("demo").innerHTML = Date();
+    const date = new Date();  //format date parsing
+    var mois= date.getFullYear();
+
+    console.log("Data",mois);
+
+    // Je vais gerer les informations avec des Objets
+    participant.nom = valeurText;
+    participant.dateinscription = date;
+
+    //participant.boutonpasse= action4.id.status;
+
+   document.getElementById("td1").innerHTML = valeurText ;
+   document.getElementById("td2").innerHTML = date;
+
+  }
+
+  function suivant(){
+// Cette fonction servira à supprimer de la liste les participants qui auront été aidé
+
+  }
+
+  function rafraichir(){
+      // Cette fonction servira à rafraichir la liste et effacer les apprenant qui auront annulé le besoins d'aide
+  }
+
+  function jepasse(){
+
+    // fonction quand le participant appuie sur le bouton je passe mon tour
   }
